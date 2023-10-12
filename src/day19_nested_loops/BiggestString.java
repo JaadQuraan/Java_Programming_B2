13cd6e72dac3105556878ff496a040dfdffa924f
Jaad Quraan <quraanjaad@gmail.com>
2023-10-11 22:01:19 -0400

    Day19 Nested Loop Intro

---
+++ src/day19_nested_loops/BiggestString.java
@@ -0,0 +1,46 @@
+package day19_nested_loops;
+
+public class BiggestString {
+    public static void main(String[] args) {
+        //Find the longest same characters
+        /*
+            loop through and get each character
+                store that into a new String --  aaa
+         */
+
+        String str = "aaabbbcccccddddeee"; // abcd
+        //            0123456789.......
+        String sub = "";  // default initial value
+        String biggest = "";
+
+        for (int i = 0; i < str.length()-1; i++) {
+
+            sub += str.charAt(i);  // sub = "aaa"; --- >  sub = "bbb";
+
+            if ( str.charAt(i) != str.charAt(i+1)) {
+
+                if (biggest.length() < sub.length() ) {
+                    biggest = sub;  // aaa  --  bbb
+                }
+
+                sub = "";
+            }
+
+        }
+
+        System.out.println(biggest);
+
+        /*
+            This is in case we have the longest at the end.
+            We can apply the if condition, but need to check where in execution to put
+
+            if (i == str.length()-2){
+              if(biggest.contains("" + str.charAt(str.length()-1))) {
+                biggest+=str.charAt(str.length()-1);
+              }
+            }
+         */
+
+    }
+
+}
